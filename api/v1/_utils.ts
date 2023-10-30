f0044817607d4a5a9ac330d075d23b7f817e8beb
sgshiw <144359810+sgshiw@users.noreply.github.com>
2023-10-30 17:38:05 +0800

    Update _utils.ts

--- api/v1/_utils.ts
+++ api/v1/_utils.ts
@@ -1,21 +1,15 @@
 import { OrganicResults, Result } from '../../src/type';
-import { Settings } from './_types';
 
-const BASE_URL = 'https://serpapi.com/search';
 
-const fetchResult = async (args: { query: string }, settings: Settings): Promise<Result> => {
-  const apiKey = settings.SERPAPI_API_KEY;
+const BASE_URL = 'https://search.wuran.link/search';
 
+const fetchResult = async (args: { query: string }): Promise<Result> => {
+  
   const { default: querystring } = await import('query-string');
 
   const params = {
-    api_key: apiKey,
-    engine: 'google',
-    gl: 'cn',
-    google_domain: 'google.com',
-    hl: 'zh-cn',
-    location: 'China',
     q: args.query,
+    max_results: 10,
   };
   const query = querystring.stringify(params);
 
@@ -25,16 +19,12 @@ const fetchResult = async (args: { query: string }, settings: Settings): Promise
 
   if (data.error) throw data;
 
-  const results = data.organic_results as OrganicResults;
+  const results = data.results;
 
-  return results.map((r) => ({
-    content: r.snippet,
-    date: r.date,
-    displayed_link: r.displayed_link,
-    favicon: r.favicon,
-    link: r.link,
-    source: r.source,
-    title: r.title,
+  return results.map((result: any) => ({
+    title: result.title,
+    content: result.body,
+    link: result.href,
   }));
 };
 
